40ca3ee964a97151517676e7aef7b574ae9215b3
FALCONKT <falcon9628@yahoo.co.jp>
2021-01-13 02:01:14 +0900

    不要になったアイテムを順次破棄　修正追加
    ユニティちゃんが通り過ぎて画面外に出たItemを直ちに破棄するようにした。破棄するItem数を増加させた。Destroy()関数を使用。  修正追加しGoal後もItemが全部消えるようにした。

--- Assets/ItemDeleter.cs
+++ Assets/ItemDeleter.cs
@@ -12,6 +12,14 @@ public class ItemDeleter : MonoBehaviour
     //★Cameraのz座標距離指定　変数
     private float difference;
 
+    //★Game終了　判定 変数
+    private bool isEnd = false;
+
+    //★UnityちゃんのObject　変数  Goal時用
+    private GameObject unitychanObjForItemD;
+    //★Goal Object　変数   Goal時用
+    private GameObject goal;
+
 
     // Use this for initialization
     void Start()
@@ -20,12 +28,19 @@ public class ItemDeleter : MonoBehaviour
         //★Camera  Objetを取得
         this.mainCamera = GameObject.Find("Main Camera");
 
-     }
+        //★UnityちゃんのObject　を取得  Goal時用
+        this.unitychanObjForItemD = GameObject.Find("unitychan");
+        //★GilaのObject　を取得  Goal時用
+        this.goal = GameObject.Find("Goal");
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+
         //★Cameraの手前奥の距離　取得
+
         this.difference = this.mainCamera.transform.position.z;
 
         //★参考　CameraとItemの位置（z座標）の差を求める
@@ -34,9 +49,20 @@ public class ItemDeleter : MonoBehaviour
 
 
         //★　もしCameraのZ座標　が　自分自身のZ座標よりも小さければ～　自分自身を削除する
+
         if (this.difference > this.transform.position.z)
         {
-             //削除命令　自分自身
+            //削除命令　自分自身
+            Destroy(this.gameObject);
+        }
+
+        //★Game終了なら　自分自身を削除する
+        //★Goal地点に到達した場合 は　背後のItem全消去
+        if (unitychanObjForItemD.transform.position.z > goal.transform.position.z)
+        {
+            this.isEnd = true;
+
+            //stateTextに CLEAR を表示（追加）
             Destroy(this.gameObject);
         }
 
